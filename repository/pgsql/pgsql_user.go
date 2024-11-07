f7d13f3ec13637951bd26259be209324ca644edf
Hamzah Miftakhuddin <hamzmk09@gmail.com>
2024-11-07 14:51:31 +0700

    fix user repository

--- repository/pgsql/pgsql_user.go
+++ repository/pgsql/pgsql_user.go
@@ -17,7 +17,7 @@ func NewPgsqlUserRepository(db *sql.DB) *pgsqlUserRepository {
 	}
 }
 
-func (r *pgsqlUserRepository) Create(ctx context.Context, user domain.User) (err error) {
+func (r *pgsqlUserRepository) Create(ctx context.Context, user *domain.User) (err error) {
 	query := "INSERT INTO users (email, password, name, created_at, updated_at) VALUES ($1, $2, $3, $4, $5)"
 	_, err = r.db.ExecContext(ctx, query, user.Email, user.Password, user.Name, user.CreatedAt, user.UpdatedAt)
 	return err
